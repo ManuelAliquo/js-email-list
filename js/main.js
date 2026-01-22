8d38e8d36c01acb0e38e83e7be9967eeb5ef2812
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-22 18:31:32 +0100

    added for loop and printed in page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
-  const email = response.data.response;
-  console.log(email);
-});
+const emailList = document.getElementById("email-list");
+
+let emailCount = 0;
+
+for (let i = 0; i < 10; i++) {
+  axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
+    const email = response.data.response;
+    console.log(++emailCount + ". " + email);
+    emailList.innerHTML += `<li>${email}</li>`;
+  });
+}
